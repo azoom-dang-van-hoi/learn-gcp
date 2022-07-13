e70b4dd3856a718eaa5d27b3851d99d0faec2b1d
azoom-dang-van-hoi <dang.van.hoi@azoom.jp>
2022-07-13 16:09:14 +0700

    :+1: Test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 const port = 80
@@ -7,13 +8,21 @@ const port = 80
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
+  console.log(req.headers, req.cookies)
+  res.cookie('test', 'demo', {
+    domain: '.hoi-demo-pomerium-trkregf5uq-an.a.run.app'
+  })
   res.sendStatus(200)
 })
 
 app.get("/api", (req, res) => {
-  console.log(req.headers)
+  console.log(req.headers, req.cookies)
+  res.cookie('test', 'demo', {
+    domain: '.hoi-demo-pomerium-trkregf5uq-an.a.run.app'
+  })
   res.sendStatus(200)
 })
 
